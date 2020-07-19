77562db74ca86083a9b2b557367b2ee9b5dbda91
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-20 00:29:15 +0530

    ADDED : Test To UnPark a vehicle from parking lot

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,11 +1,29 @@
 import com.bridgelabz.parkinglot.service.ParkingLot;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 public class ParkingLotTest {
+    Object vehicle;
+    ParkingLot parkingLotSystem;
+
+    @Before
+    public void setUp() {
+        new Object();
+        new ParkingLot();
+    }
+
     @Test
     public void givenAVehicle_IfParked_ShouldReturnTrue() {
-        ParkingLot parkingLot=new ParkingLot();
-        boolean isParked = parkingLot.park(new Object());
+        boolean isParked = parkingLotSystem.park(vehicle);
         Assert.assertTrue(isParked);
     }
+
+    @Test
+    public void givenAVehicle_IfUnParked_ShouldReturnTrue() {
+        parkingLotSystem.park(vehicle);
+        boolean isUnParked = parkingLotSystem.UnPark(vehicle);
+        Assert.assertFalse(isUnParked);
+    }
 }
+
+
