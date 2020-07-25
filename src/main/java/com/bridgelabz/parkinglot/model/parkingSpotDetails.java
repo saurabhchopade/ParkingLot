ffe54803b8f508c559b56b54ea056516e5410339
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-25 08:45:52 +0530

    REFACTORED : Class name which is in model

--- src/main/java/com/bridgelabz/parkinglot/model/parkingSpotDetails.java
+++ src/main/java/com/bridgelabz/parkinglot/model/parkingSpotDetails.java
@@ -2,12 +2,12 @@ package com.bridgelabz.parkinglot.model;
 import com.bridgelabz.parkinglot.enums.DriverType;
 
 import java.time.LocalDateTime;
-public class slotDetails {
+public class parkingSpotDetails {
     public LocalDateTime vehicleParkingTime;
     public String vehicleNumber;
     public DriverType driverType;
 
-    public slotDetails(String vehicleNumber, DriverType driverType) {
+    public parkingSpotDetails(String vehicleNumber, DriverType driverType) {
         this.vehicleParkingTime = LocalDateTime.now();
         this.vehicleNumber = vehicleNumber;
         this.driverType = driverType;
